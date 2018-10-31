3a6a29a37d32ccd8cb0d39ea4ef620239c8eaef9
julenZarate <39679963+julenZarate@users.noreply.github.com>
2018-10-31 08:49:27 +0100

    Update IDataImplementation.java

--- Factoria_Datos/src/logic/IDataImplementation.java
+++ Factoria_Datos/src/logic/IDataImplementation.java
@@ -84,7 +84,7 @@ public class IDataImplementation implements IData  {
 
 	public synchronized UserBean userLogin(UserBean user) throws UserNotExistException, PasswordNotOkException, SQLException {
 		ResultSet rs = null;
-		ArrayList <UserBean> usuarios = new ArraylList <UserBean>();
+		ArrayList <UserBean> usuarios = new ArrayList <UserBean>();
 		try {
 			connect();
 			String select="select * from usuarios where login=? and password=?";
